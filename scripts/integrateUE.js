774789a67111523a968d3a1b3b7045ab65c721d0
Jaykumar Patil <jaykumar.patil@mphasis.com>
2024-10-21 01:23:59 +0530

    Added script to refelct the published changes to the site

---
+++ scripts/integrateUE.js
@@ -0,0 +1,63 @@
+(async function(){
+    const publishHostUrl = document.currentScript?.dataset?.publishHost;
+    if (!publishHostUrl) {
+        console.error('Attribute "data-publish-host" is missing');
+        return;
+    }
+    // const publishHostUrl = 'https://publish-p140494-e1434146.adobeaemcloud.com/';
+    const authorHostMeta = document.querySelector('meta[name="urn:adobe:aue:system:aemconnection"]');
+    if(!authorHostMeta) {
+        console.error('Meta tag with content "urn:adobe:aue:system:aemconnection" is required');
+        return;
+    }
+    const authorHostUrl = (authorHostMeta?.content || "")?.split('aem:').pop();
+
+    async function fetchData(path) {
+        let errorMessage;
+        let data;
+
+        try {
+            const response = await fetch(`${path}?${Date.now()}`);
+            data = await response.json();
+            if (!response.ok) {
+                console.error('apiError data', data);
+                throw new Error(`Error with status ${response.status} while fetching the request. Please check the logs`)
+            }
+            // return data;
+        } catch (error) {
+            console.error(error.message);
+        }
+
+        return data?.data;
+    }
+
+    const nodes = document.querySelectorAll('*[data-aue-resource]');
+    // console.log('Nodes-->', nodes);
+    const bannerHeadline = nodes[0];
+    
+    console.log('bannerHeadline-->', bannerHeadline);
+    const { graphKey, graphPath } = bannerHeadline?.dataset;
+
+    if (!graphPath) {
+        console.error('graphPath is required');
+        return;
+    }
+
+    const data = await fetchData(`${publishHostUrl}graphql/execute.json/${graphPath}`);
+
+    const item = data?.[graphKey]?.item;
+    const contentFragmentKeys = Object.keys(item).filter(key => !key.startsWith('_'));
+
+    for (let i = 0; i <= contentFragmentKeys.length; i++) {
+        const key = contentFragmentKeys[i];
+        // check if this needs to be changed to data-aue-label
+        const contentNode = bannerHeadline.querySelector(`*[data-aue-prop="${key}"]`);
+        if (!contentNode) continue;
+
+        if (contentNode?.dataset?.aueType === 'text') {
+            contentNode.innerText = item?.[key];
+        } else if (contentNode?.dataset?.aueType === "richtext") {
+            contentNode.innerHTML = item?.[key]?.html;
+        }
+    }
+})()
